9e84cb82f0867d782d75dfc4439812377fb973ae
Nish1779 <64070976+Nish1779@users.noreply.github.com>
2022-08-11 10:57:36 -0400

    Initial commit

---
+++ android/app/src/main/java/com/example/flutterstarting/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutterstarting;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('button Clicked!');
+        },
+      ),
+      body: Text('Hello'),
+    );
+  }
+}
